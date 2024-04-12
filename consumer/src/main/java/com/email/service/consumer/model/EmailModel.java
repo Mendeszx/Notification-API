12bd3443f24aa678f3767fd2c4642c15f520766a
Guilherme Mendes <guilherme_mendes2018@outlook.com>
2024-04-12 01:05:52 -0300

    primeiro envio de email

---
+++ consumer/src/main/java/com/email/service/consumer/model/EmailModel.java
@@ -0,0 +1,25 @@
+package com.email.service.consumer.model;
+
+import com.email.service.consumer.model.enums.StatusEmail;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "email")
+public class EmailModel implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+    private String ownerRef;
+    private String emailFrom;
+    private String emailTo;
+    private String subject;
+    private String text;
+    private LocalDateTime sendDateEmail;
+    private StatusEmail statusEmail;
+}
--- consumer/src/main/java/com/email/service/consumer/model/dto/EnviarEmailRequestDTO.java
+++ consumer/src/main/java/com/email/service/consumer/model/dto/EnviarEmailRequestDTO.java
@@ -1,15 +1,19 @@
 package com.email.service.consumer.model.dto;
 
+import jakarta.persistence.Column;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
+@Data
 public class EnviarEmailRequestDTO {
 
-    @NotBlank
-    @NotNull
-    private String email;
+    private String ownerRef;
+    private String emailFrom;
+    private String emailTo;
+    private String subject;
+    @Column(columnDefinition = "TEXT")
+    private String text;
 }
